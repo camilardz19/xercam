0c8d587518ac2e8bbd4284687599b9c457f1ea7d
camilardz19 <rodriguezsalinascamila19@gmail.com>
2026-04-20 20:19:25 -0600

    agrege getters

--- clase.py
+++ clase.py
@@ -5,3 +5,19 @@ class Capibara:
         self.__peso = peso
         self.__color = color
         self.__habitat = habitat
+    
+    
+    def get_nombre(self):
+        return self.__nombre
+
+    def get_edad(self):
+        return self.__edad
+
+    def get_peso(self):
+        return self.__peso
+
+    def get_color(self):
+        return self.__color
+
+    def get_habitat(self):
+        return self.__habitat
